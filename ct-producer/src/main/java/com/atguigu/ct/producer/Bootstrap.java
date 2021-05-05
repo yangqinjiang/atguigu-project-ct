a912831ea3bd53172f14ad94dd6919c39c69c257
yangqinjiang <531553280@qq.com>
2021-05-05 13:51:40 +0800

    完善[数据生产]的脚本, 代码等

--- ct-producer/src/main/java/com/atguigu/ct/producer/Bootstrap.java
+++ ct-producer/src/main/java/com/atguigu/ct/producer/Bootstrap.java
@@ -9,12 +9,19 @@ import com.atguigu.ct.producer.io.LocalFileDataOut;
 public class Bootstrap {
     public static void main(String[] args) throws  Exception {
 
+        if(args.length < 2){
+            System.out.println("系统参数不正确，请按照指定格式传递：java -jar Produce.jar path1 path2 ");
+            System.exit(1);
+        }
         //构建生产者对象
         Producer producer = new LocalFileProducer();
 
         //设置生产者的数据来源, 输出
-        producer.setIn(new LocalFileDataIn("f:\\contact.log"));
-        producer.setOut(new LocalFileDataOut("f:\\contact-out.log"));
+//        producer.setIn(new LocalFileDataIn("f:\\contact.log"));
+//        producer.setOut(new LocalFileDataOut("f:\\contact-out.log"));
+        // 使用参数
+        producer.setIn(new LocalFileDataIn(args[0]));
+        producer.setOut(new LocalFileDataOut(args[1]));
 
         //生产数据
         producer.produce();
